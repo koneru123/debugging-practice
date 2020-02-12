d719114789af5babc7b7d8d83bef2d0940236bd3
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-12 10:28:19 -0800

    Adding debugging steps to find-object.js

--- find-object.js
+++ find-object.js
@@ -1,3 +1,5 @@
+Array.from(someArray)
+
 /*
   Write a function, findObject, that takes an array of objects, a key,
   and a value, and returns the first object with a key/value pair that
@@ -9,15 +11,29 @@
     { name: 'Shadow', breed: 'Husky' },
     { name: 'Molly', breed: 'Collie' }
   ];
-  findObject(dogs, 'breed', 'Husky') 
+  findObject(dogs, 'breed', 'Husky')
   should return { name: 'Scout', breed: 'Husky' }
 */
 
 var findObject = function(list, key, value) {
   for (var i = 0; i < list.length; i++) {
+    // here we need to also loop through individual object inside the array
+    // list[i].key is undefined in this case and since we are comparing undefined to a value this condition is not going to be evaluated.
     if (list[i].key === value) {
       return list[i];
     }
   }
   return null;
 };
+
+var dogs = [
+  { name: 'Scout', breed: 'Husky' },
+  { name: 'Lucky', breed: 'Beagle' },
+  { name: 'Shadow', breed: 'Husky' },
+  { name: 'Molly', breed: 'Collie' }
+];
+//This would always evaluate to null
+//console.log(findObject(dogs, 'breed', 'Husky')); //should return { name: 'Scout', breed: 'Husky' }
+
+
+
